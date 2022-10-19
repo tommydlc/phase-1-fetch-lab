c53303250e15fa0c150691c7149380519a628af7
Tommy De La Cruz <tommydlc08@gmail.com>
2022-10-19 15:47:18 -0400

    commit tree 12feb2d9d3e8e020aa23db1f633d192df6b5b370

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+.then((resp) => resp.json())
+.then((json) => (json));
   
 }
 
@@ -9,9 +12,14 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+fetchBooks();
